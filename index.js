a7199841549a9b4709ba3e99ef23ad8d449157d7
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-02-02 14:13:17 +0200

    Name Changes

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
             <span>${data.name}</span>
         `
         document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
+            <p> Current Price : $${data.market_data.current_price.usd}</p>
+            <p> 24h High : $${data.market_data.high_24h.usd}</p>
+            <p> 24h Low : $${data.market_data.low_24h.usd}</p>
         `
     })
     .catch(err => console.error(err))
